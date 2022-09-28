8e8924106337edd791e2c2d3cd9d5c7f87626f2a
David Miguel Gómez Macías <davidgomezmacias@gmail.com>
2022-09-28 16:15:33 -0500

    ADD: Método para conectar desde un servicio a otro.

--- libraries/BuriPHP/Service.class.php
+++ libraries/BuriPHP/Service.class.php
@@ -15,6 +15,8 @@ namespace Libraries\BuriPHP;
 
 use Libraries\BuriPHP\Helpers\HelperArray;
 use Libraries\BuriPHP\Helpers\HelperFile;
+use Libraries\BuriPHP\Helpers\HelperLog;
+use Libraries\BuriPHP\Helpers\HelperValidate;
 use Libraries\BuriPHP\Interfaces\iService;
 
 class Service implements iService
@@ -77,5 +79,40 @@ class Service implements iService
         }
 
         return $request;
+    }
+
+    /**
+     * Conecta un servico de otro módulo.
+     */
+    final public function serviceShared($module, $service)
+    {
+        try {
+            /**
+             * Verifica que exista el módulo.
+             */
+            if (!HelperValidate::isDir(PATH_MODULES . $module)) {
+                $exceptionMsg = "No existe el module: " . PATH_MODULES . $module;
+
+                HelperLog::saveError($exceptionMsg);
+                throw new \Exception($exceptionMsg);
+            }
+
+            /**
+             * Verifica que exista el servicio.
+             */
+            if (!HelperFile::exists(PATH_MODULES . $module . DS . $service . SERVICE_PHP)) {
+                $exceptionMsg = "No existe el service: " . PATH_MODULES . $module . DS . $service . SERVICE_PHP;
+
+                HelperLog::saveError($exceptionMsg);
+                throw new \Exception($exceptionMsg);
+            } else {
+                require PATH_MODULES . $module . DS . $service . SERVICE_PHP;
+
+                $service = '\Services\\' . $service;
+                return new $service();
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 }
