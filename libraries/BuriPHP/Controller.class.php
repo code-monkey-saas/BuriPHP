2f96ae31d954996fcc39bd1ca6c0b08a78f3984b
David Miguel Gómez Macías <davidgomezmacias@gmail.com>
2024-03-23 09:17:53 -0500

    Update get payload

--- libraries/BuriPHP/Controller.class.php
+++ libraries/BuriPHP/Controller.class.php
@@ -4,7 +4,7 @@
  * @package BuriPHP.Libraries
  *
  * @since 1.0
- * @version 2.3
+ * @version 2.4
  * @license You can see LICENSE.txt
  *
  * @author David Miguel Gómez Macías < davidgomezmacias@gmail.com >
@@ -124,22 +124,49 @@ class Controller implements iController
     }
 
     /**
+     * @deprecated This function is deprecated and should not be used.
      * Obtiene la data e imagenes enviada por post.
      */
     final public function getPost()
     {
+        return self::getPayload();
+    }
+
+    /**
+     * @deprecated This function is deprecated and should not be used.
+     * Obtiene la data enviada por put.
+     */
+    final public function getPut()
+    {
+        return self::getPayload();
+    }
+
+    /**
+     * @deprecated This function is deprecated and should not be used.
+     * Obtiene la data e imagenes enviada por patch.
+     */
+    final public function getPatch()
+    {
+        return self::getPayload();
+    }
+
+
+    /**
+     * Obtiene los datos de la solicitud HTTP y los devuelve como un arreglo.
+     *
+     * @return array Los datos de la solicitud HTTP.
+     */
+    final public function getPayload()
+    {
         $request = [];
 
-        if (!empty($_POST)) {
-            // when using application/x-www-form-urlencoded or multipart/form-data as the HTTP Content-Type in the request
-            // NOTE: if this is the case and $_POST is empty, check the variables_order in php.ini! - it must contain the letter P
-            $request = array_merge($request, $_POST);
+        $rawData = self::parseRawData();
+        if (!empty($rawData)) {
+            $request = array_merge($request, $rawData);
         }
 
-        // when using application/json as the HTTP Content-Type in the request 
-        $post = json_decode(file_get_contents('php://input'), true);
-        if (json_last_error() == JSON_ERROR_NONE) {
-            $request = array_merge($request, $post);
+        if (!empty($_POST)) {
+            $request = array_merge($request, $_POST);
         }
 
         if (isset($_FILES) && !empty($_FILES)) {
@@ -150,328 +177,87 @@ class Controller implements iController
     }
 
     /**
-     * Obtiene la data enviada por put.
+     * Función privada para analizar los datos sin procesar.
+     *
+     * Lee los datos sin procesar de la entrada PHP y los procesa según su tipo.
+     * Si los datos son JSON, los decodifica y devuelve un array asociativo.
+     * Si los datos son una cadena de consulta, los analiza y devuelve un array asociativo.
+     * Si los datos son una carga de archivo, los guarda temporalmente y actualiza la variable global $_FILES.
+     *
+     * @return array Los datos procesados.
      */
-    final public function getPut()
+    private static function parseRawData()
     {
-        // Fetch content and determine boundary
-        $raw_data = file_get_contents('php://input');
+        $_raw_data = fopen("php://input", "r");
+        $raw_data = '';
+
+        while ($chunk = fread($_raw_data, 1024))
+            $raw_data .= $chunk;
+
+        fclose($_raw_data);
+
         $boundary = substr($raw_data, 0, strpos($raw_data, "\r\n"));
 
         if (empty($boundary)) {
-            return json_decode($boundary, true);
+            json_decode($raw_data);
+            if (json_last_error() === JSON_ERROR_NONE) {
+                return json_decode($raw_data, true);
+            } else {
+                parse_str($raw_data, $data);
+                return $data;
+            }
         }
 
-        // Fetch each part
         $parts = array_slice(explode($boundary, $raw_data), 1);
         $data = array();
 
         foreach ($parts as $part) {
-            // If this is the last part, break
             if ($part == "--\r\n") break;
 
-            // Separate content from headers
             $part = ltrim($part, "\r\n");
             list($raw_headers, $body) = explode("\r\n\r\n", $part, 2);
 
-            // Parse the headers list
             $raw_headers = explode("\r\n", $raw_headers);
             $headers = array();
             foreach ($raw_headers as $header) {
                 list($name, $value) = explode(':', $header);
-
-                if (!is_null($value)) {
-                    $headers[strtolower($name)] = ltrim($value, ' ');
-                }
+                $headers[strtolower($name)] = ltrim($value, ' ');
             }
 
-            // Parse the Content-Disposition to get the field name, etc.
             if (isset($headers['content-disposition'])) {
                 $filename = null;
+                $tmp_name = null;
                 preg_match(
                     '/^(.+); *name="([^"]+)"(; *filename="([^"]+)")?/',
                     $headers['content-disposition'],
                     $matches
                 );
                 list(, $type, $name) = $matches;
-                isset($matches[4]) and $filename = $matches[4];
 
-                // handle your fields here
-                switch ($name) {
-                        // this is a file upload
-                    case 'userfile':
-                        file_put_contents($filename, $body);
-                        break;
+                if (isset($matches[4])) {
+                    if (isset($_FILES[$matches[2]])) {
+                        continue;
+                    }
 
-                        // default for all other files is to populate $data
-                    default:
-                        $data[$name] = substr($body, 0, strlen($body) - 2);
-                        break;
+                    $filename = $matches[4];
+
+                    $filename_parts = pathinfo($filename);
+                    $tmp_name = tempnam(ini_get('upload_tmp_dir'), $filename_parts['filename']);
+
+                    $_FILES[$matches[2]] = array(
+                        'error' => 0,
+                        'name' => $filename,
+                        'tmp_name' => $tmp_name,
+                        'size' => strlen($body),
+                        'type' => $value
+                    );
+
+                    file_put_contents($tmp_name, $body);
+                } else {
+                    $data[$name] = substr($body, 0, strlen($body) - 2);
                 }
             }
         }
-
         return $data;
-    }
-
-    /**
-     * Obtiene la data e imagenes enviada por patch.
-     */
-    final public function getPatch()
-    {
-        $request = [];
-
-        // when using application/json as the HTTP Content-Type in the request 
-        $patch = json_decode(file_get_contents('php://input'), true);
-        if (json_last_error() == JSON_ERROR_NONE) {
-            $request = array_merge($request, $patch);
-        }
-
-        if (isset($_FILES) && !empty($_FILES)) {
-            $request = array_merge($request, $_FILES);
-        }
-
-        return $request;
-    }
-
-    /**
-     * Obtiene la data e imagenes enviada general.
-     */
-    final public function getPayload()
-    {
-        $result = self::processPayload();
-
-        return array_merge($result['variables'], $result['files']);
-    }
-
-    private static function processPayload()
-    {
-        $content_parts = explode(';', $_SERVER['CONTENT_TYPE'] ?? 'application/x-www-form-urlencoded');
-
-        $boundary = '';
-        $encoding = '';
-
-        $content_type = array_shift($content_parts);
-
-        foreach ($content_parts as $part) {
-            if (strpos($part, 'boundary') !== false) {
-                $part = explode('=', $part, 2);
-                if (!empty($part[1])) {
-                    $boundary = '--' . $part[1];
-                }
-            } elseif (strpos($part, 'charset') !== false) {
-                $part = explode('=', $part, 2);
-                if (!empty($part[1])) {
-                    $encoding = $part[1];
-                }
-            }
-            if ($boundary !== '' && $encoding !== '') {
-                break;
-            }
-        }
-
-        if ($content_type == 'multipart/form-data') {
-            return self::fetchFromMultipart($boundary);
-        }
-
-        // can be handled by built in PHP functionality
-        $content = file_get_contents('php://input');
-
-        $variables = json_decode($content);
-
-        if (empty($variables)) {
-            parse_str($content, $variables);
-        }
-
-        return ['variables' => $variables, 'files' => []];
-    }
-
-    private static function fetchFromMultipart(string $boundary)
-    {
-        $result = ['variables' => [], 'files' => []];
-
-        $stream = fopen('php://input', 'rb');
-
-        $sanity = fgets($stream, strlen($boundary) + 5);
-
-        // malformed file, boundary should be first item
-        if (rtrim($sanity) !== $boundary) {
-            return $result;
-        }
-
-        $raw_headers = '';
-
-        while (($chunk = fgets($stream)) !== false) {
-            if ($chunk === $boundary) {
-                continue;
-            }
-
-            if (!empty(trim($chunk))) {
-                $raw_headers .= $chunk;
-                continue;
-            }
-
-            $result = self::parseRawHeader($stream, $raw_headers, $boundary, $result);
-            $raw_headers = '';
-        }
-
-        fclose($stream);
-
-        return $result;
-    }
-
-    private static function parseRawHeader($stream, string $raw_headers, string $boundary, array $result)
-    {
-        $variables = $result['variables'];
-        $files     = $result['files'];
-
-        $headers = [];
-
-        foreach (explode("\r\n", $raw_headers) as $header) {
-            if (strpos($header, ':') === false) {
-                continue;
-            }
-            list($name, $value) = explode(':', $header, 2);
-            $headers[strtolower($name)] = ltrim($value, ' ');
-        }
-
-        if (!isset($headers['content-disposition'])) {
-            return ['variables' => $variables, 'files' => $files];
-        }
-
-        if (!preg_match('/^(.+); *name="([^"]+)"(; *filename="([^"]+)")?/', $headers['content-disposition'], $matches)) {
-            return ['variables' => $variables, 'files' => $files];
-        }
-
-        $name     = $matches[2];
-        $filename = $matches[4] ?? '';
-
-        if (!empty($filename)) {
-            $files[$name] = self::fetchFileData($stream, $boundary, $headers, $filename);
-            return ['variables' => $variables, 'files' => $files];
-        } else {
-            $variables = self::fetchVariables($stream, $boundary, $name, $variables);
-        }
-
-        return ['variables' => $variables, 'files' => $files];
-    }
-
-    private static function fetchFileData($stream, string $boundary, array $headers, string $filename)
-    {
-        $error = UPLOAD_ERR_OK;
-
-        if (isset($headers['content-type'])) {
-            $tmp = explode(';', $headers['content-type']);
-            $contentType = $tmp[0];
-        } else {
-            $contentType = 'unknown';
-        }
-
-        $tmpnam     = tempnam(ini_get('upload_tmp_dir'), 'php');
-        $fileHandle = fopen($tmpnam, 'wb');
-
-        if ($fileHandle === false) {
-            $error = UPLOAD_ERR_CANT_WRITE;
-        } else {
-            $lastLine = NULL;
-            while (($chunk = fgets($stream, 8096)) !== false && strpos($chunk, $boundary) !== 0) {
-                if ($lastLine !== NULL) {
-                    if (fwrite($fileHandle, $lastLine) === false) {
-                        $error = UPLOAD_ERR_CANT_WRITE;
-                        break;
-                    }
-                }
-                $lastLine = $chunk;
-            }
-
-            if ($lastLine !== NULL && $error !== UPLOAD_ERR_CANT_WRITE) {
-                if (fwrite($fileHandle, rtrim($lastLine, "\r\n")) === false) {
-                    $error = UPLOAD_ERR_CANT_WRITE;
-                }
-            }
-        }
-
-        return [
-            'name'     => $filename,
-            'type'     => $contentType,
-            'tmp_name' => $tmpnam,
-            'error'    => $error,
-            'size'     => filesize($tmpnam)
-        ];
-    }
-
-    private static function fetchVariables($stream, string $boundary, string $name, array $variables)
-    {
-        $fullValue = '';
-        $lastLine = NULL;
-
-        while (($chunk = fgets($stream)) !== false && strpos($chunk, $boundary) !== 0) {
-            if ($lastLine !== NULL) {
-                $fullValue .= $lastLine;
-            }
-
-            $lastLine = $chunk;
-        }
-
-        if ($lastLine !== NULL) {
-            $fullValue .= rtrim($lastLine, "\r\n");
-        }
-
-        if (isset($headers['content-type'])) {
-            $encoding = '';
-
-            foreach (explode(';', $headers['content-type']) as $part) {
-                if (strpos($part, 'charset') !== false) {
-                    $part = explode($part, '=', 2);
-                    if (isset($part[1])) {
-                        $encoding = $part[1];
-                    }
-                    break;
-                }
-            }
-
-            if ($encoding !== '' && strtoupper($encoding) !== 'UTF-8' && strtoupper($encoding) !== 'UTF8') {
-                $tmp = mb_convert_encoding($fullValue, 'UTF-8', $encoding);
-                if ($tmp !== false) {
-                    $fullValue = $tmp;
-                }
-            }
-        }
-
-        $fullValue = $name . '=' . $fullValue;
-
-        $tmp = [];
-        // parse_str($fullValue, $tmp);
-        $tmp[HelperString::getLeftString($fullValue, '=')] = HelperString::getRightStringBack($fullValue, '=');
-
-        return self::expandVariables(explode('[', $name), $variables, $tmp);
-    }
-
-    private static function expandVariables(array $names, $variables, array $values)
-    {
-        if (!is_array($variables)) {
-            return $values;
-        }
-
-        $name = rtrim(array_shift($names), ']');
-        if ($name !== '') {
-            $name = $name . '=p';
-
-            $tmp = [];
-            parse_str($name, $tmp);
-
-            $tmp  = array_keys($tmp);
-            $name = reset($tmp);
-        }
-
-        if ($name === '') {
-            $variables[] = reset($values);
-        } elseif (isset($variables[$name]) && isset($values[$name])) {
-            $variables[$name] = self::expandVariables($names, $variables[$name], $values[$name]);
-        } elseif (isset($values[$name])) {
-            $variables[$name] = $values[$name];
-        }
-
-        return $variables;
     }
 }
